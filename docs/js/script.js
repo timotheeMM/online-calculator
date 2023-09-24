9124d07300f4d2bd1a4e8969f9f7f80888ce958b
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-09-24 13:31:58 +0200

    write the javascript code

---
+++ docs/js/script.js
@@ -0,0 +1,41 @@
+let display = document.getElementById('display');
+let expression = '';
+
+function appendNumber(num) {
+    expression += num;
+    display.value = expression;
+}
+
+function appendOperator(op) {
+    expression += op;
+    display.value = expression;
+}
+
+function appendDecimal() {
+    if (!expression.includes('.')) {
+        expression += '.';
+        display.value = expression;
+    }
+}
+
+function clearDisplay() {
+    expression = '';
+    display.value = '';
+}
+
+function deleteLast() {
+    expression = expression.slice(0, -1);
+    display.value = expression;
+}
+
+function calculate() {
+    try {
+        let modifiedExpression = expression.replace(/×/g, '*').replace(/÷/g, '/');
+        let result = eval(modifiedExpression);
+        display.value = result;
+        expression = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+        expression = '';
+    }
+}
